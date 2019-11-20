e9731bd262432ddef3edc12350f416ff4df65764
maximizer2k20 <44780075+maximizer2k20@users.noreply.github.com>
2019-11-20 09:32:50 -0600

    Update rootsofanequation.c

--- rootsofanequation.c
+++ rootsofanequation.c
@@ -4,7 +4,7 @@ main()
 {
 	float a,b,c,r1,r2;
 	scanf(“%f%f%f”, &a, &b, &c);
-	r1=(-b + sqrt(b*b-4*a*c))/(2*a);
-	r2=(-b1 - sqrt(b*b-4*a*c))/(2*a);
+	r1=(-b + sqrt(b*b-4*a*c))/(2*a); /* equation for finding first root */
+	r2=(-b1 - sqrt(b*b-4*a*c))/(2*a); /* equation for finding first root */
 	printf(“root1 = %f, root2= %f \n”, r1,r2);
 } 
