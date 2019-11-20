90acdb7457f51164e5571ca99d8ceee95113aa61
maximizer2k20 <44780075+maximizer2k20@users.noreply.github.com>
2019-11-20 09:28:44 -0600

    Quadratic equation roots

---
+++ rootsofanequation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<math.h>
+main()
+{
+	float a,b,c,r1,r2;
+	scanf(“%f%f%f”, &a, &b, &c);
+	r1=(-b + sqrt(b*b-4*a*c))/(2*a);
+	r2=(-b1 - sqrt(b*b-4*a*c))/(2*a);
+	printf(“root1 = %f, root2= %f \n”, r1,r2);
+} 
